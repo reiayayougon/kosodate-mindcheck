34cf411b66b7cdeff7b78ff80824da82a6d89afa
reiayayougon <ishida.ayako0306@gmail.com>
2023-09-25 13:29:20 +0900

    Fix: 不要な記述削除

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,9 +7,6 @@ import { application } from "./application"
 import ClearCommentController from "./clear_comment_controller"
 application.register("clear-comment", ClearCommentController)
 
-import CommentEditController from "./comment_edit_controller"
-application.register("comment-edit", CommentEditController)
-
 import PreviewController from "./preview_controller"
 application.register("preview", PreviewController)
 
