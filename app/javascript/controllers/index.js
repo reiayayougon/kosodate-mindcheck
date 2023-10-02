81f76be153b82a80a95727ce301521ac825cb556
reiayayougon <ishida.ayako0306@gmail.com>
2023-10-02 16:26:24 +0900

    Remove: 余計なファイル削除

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,9 +7,6 @@ import { application } from "./application"
 import AlbamModalController from "./albam_modal_controller"
 application.register("albam-modal", AlbamModalController)
 
-import ClearCommentController from "./clear_comment_controller"
-application.register("clear-comment", ClearCommentController)
-
 import ModalController from "./modal_controller"
 application.register("modal", ModalController)
 
