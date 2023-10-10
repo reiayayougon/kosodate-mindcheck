a644dcbb26fb692d43d30fca16f37abbaa7c66eb
reiayayougon <ishida.ayako0306@gmail.com>
2023-10-10 11:50:24 +0900

    Add: いいね機能テスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:like) { create(:like, user: user, post: post) }
+
+  it 'ユーザーが投稿にいいねできること' do
+    expect(like).to be_valid
+    expect(user.likes).to include(like)
+    expect(post.likes).to include(like)
+  end
 end
+  
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,12 +35,11 @@ RSpec.configure do |config|
   
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  Capybara.default_driver = :selenium_chrome
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
