e5409e5d4854e3561cb13aeb87c06d2f930d9ba7
reiayayougon <ishida.ayako0306@gmail.com>
2023-11-08 02:53:25 +0900

    本番環境actioncableCSRF設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_cable.allowed_request_origins = [
-    "https://kosodate-mind-check.com"
-  ]
+  
+  config.action_cable.disable_request_forgery_protection = true
 end
