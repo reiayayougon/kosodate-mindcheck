b1a4231eecb74930fd45a95ec6038ae09f4e150f
reiayayougon <ishida.ayako0306@gmail.com>
2023-11-08 10:23:15 +0900

    Fix: 本番環境actioncable設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_cable.disable_request_forgery_protection = true
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://kosodate-mind-check.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://kosodate-mind-check.com', 'https://kosodate-mind-check-28806aa22838.herokuapp.com']
+
 end
