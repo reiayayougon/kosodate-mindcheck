2f2c3919cf324830193790c46a125bea58d93631
reiayayougon <ishida.ayako0306@gmail.com>
2023-11-13 14:34:12 +0900

    Fix: README修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-# 10.times do |i|
-#     User.find_or_create_by(email: "user#{i + 1}@example.com") do |user|
-#         user.name = "User #{i + 1}"
-#     end
-# end
+10.times do |i|
+    User.find_or_create_by(email: "user#{i + 1}@example.com") do |user|
+        user.name = "User #{i + 1}"
+    end
+end
 
 
 category_names = ["保育園・幼稚園", "食事", "健康・病気・怪我", "義理母・義理父", "妻・夫", "友人", "ママ友", "乳児期", "幼児期初期〜後期", "児童期", "青年期", "お金", "その他"] 
@@ -10,18 +10,16 @@ category_names.each do |name|
     category = Category.find_or_create_by(name: name)
 end
 
-# 20.times do |index|
-#     post = Post.new(
-#         user: User.offset(rand(User.count)).first,
-#         content: "本文#{index}",
-#     )
+20.times do |index|
+    post = Post.new(
+        user: User.offset(rand(User.count)).first,
+        content: "本文#{index}",
+    )
     
-#     random_category = Category.offset(rand(Category.count)).first
-#     post.category = random_category
-#     post.save!
-# end
-
-
+    random_category = Category.offset(rand(Category.count)).first
+    post.category = random_category
+    post.save!
+end
 # questions = [
 #     { content: "妻(夫)の育児に言いたいことがあるが言えない", category_name: "妻・夫" },
 #     { content: "子供のイヤイヤ期に怒鳴ってばかり", category_name: "幼児期初期〜後期" },
